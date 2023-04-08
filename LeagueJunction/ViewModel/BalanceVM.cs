070e8645b2a3bbd91ee3be9bedf14a6e0ba3e771
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-04-08 18:12:13 +0200

    feat: Example code for buttons and file dialog

---
+++ LeagueJunction/ViewModel/BalanceVM.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueJunction.ViewModel
+{
+    public class BalanceVM : ObservableObject
+    {
+        private string SelectedFileName { get; set; }
+
+        public RelayCommand SelectFileCommand { get; private set; }
+        public RelayCommand GenerateTeamsCommand { get; private set; }
+        public bool IsGenerateTeamsCommandEnabled { get; private set; }
+
+        public BalanceVM()
+        {
+            SelectFileCommand = new RelayCommand(SelectFileDialog);
+            GenerateTeamsCommand = new RelayCommand(GenerateTeams);
+            IsGenerateTeamsCommandEnabled = false;
+        }
+
+        // Proxy
+        private void SelectFileDialog()
+        {
+            // Source: https://wpf-tutorial.com/dialogs/the-openfiledialog/
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SelectedFileName = openFileDialog.FileName;
+                Console.WriteLine(SelectedFileName);
+                IsGenerateTeamsCommandEnabled = true;
+                OnPropertyChanged(nameof(IsGenerateTeamsCommandEnabled));
+            }
+
+        }
+
+        private void GenerateTeams()
+        {
+
+        }
+    }
+}
