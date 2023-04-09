ff166233910b72dfe52a2a98c3fd774243392803
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-04-09 15:44:27 +0200

    feat: registration form reader

---
+++ LeagueJunction/Repository/CsvRegistrationReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace LeagueJunction.Repository
+{
+    public class RawFormsAnswer
+    {
+		[Index(0)]
+		public string Timestamp { get; set; }
+		[Index(1)]
+		public string DiscordUsername { get; set; }
+		[Index(2)]
+		public string MainOpGG { get; set; }
+		[Index(3)]
+		public string RegionOpGG { get; set; }
+		[Index(4)]
+		public string PreferredRoles { get; set; }
+    }
+
+    public class CsvRegistrationReader
+    {
+        static public List<RawFormsAnswer> GetRawFormsAnswers(string file_path)
+        {
+			// For some reason some lines give baddata
+			var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				BadDataFound = null
+			};
+			using (var reader = new StreamReader(file_path))
+			using (var csv = new CsvReader(reader, config))
+			{
+				var records = csv.GetRecords<RawFormsAnswer>().ToList();
+                return records;
+			}
+		}
+    }
+}
