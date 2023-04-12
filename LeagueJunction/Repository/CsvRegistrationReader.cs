be991cb1185f909981b1243bb29041779cc36687
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-04-12 22:17:40 +0200

    feat: defined repos interface

--- LeagueJunction/Repository/CsvRegistrationReader.cs
+++ LeagueJunction/Repository/CsvRegistrationReader.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+using LeagueJunction.Model;
 
 namespace LeagueJunction.Repository
 {
@@ -25,7 +26,7 @@ namespace LeagueJunction.Repository
 		public string PreferredRoles { get; set; }
     }
 
-    public class CsvRegistrationReader
+    public class CsvRegistrationReader : IPlayerRepository
     {
         static public List<RawFormsAnswer> GetRawFormsAnswers(string file_path)
         {
@@ -41,5 +42,17 @@ namespace LeagueJunction.Repository
                 return records;
 			}
 		}
+
+        public List<Player> GetPlayers(string sourceFile)
+        {
+			List<Player> players = new List<Player>();
+			var rawAnswers = GetRawFormsAnswers(sourceFile);
+			foreach (var rawAnswer in rawAnswers) 
+			{
+				//var player = new Player();
+                //players.Add(player);
+			}
+			return players;
+        }
     }
 }
---
+++ LeagueJunction/Repository/IPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueJunction.Model;
+
+namespace LeagueJunction.Repository
+{
+    public interface IPlayerRepository
+    {
+        List<Player> GetPlayers(string sourceFile);
+    }
+}
